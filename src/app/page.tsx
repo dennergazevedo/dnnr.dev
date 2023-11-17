d33e6bc85e5f09dfca8c055606171c151d1f3cc6
Denner Azevedo <denner.gomes@ciahering.com.br>
2023-11-17 12:31:51 -0300

    fix: profile image position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ const Home: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className='flex flex-col justify-start items-center w-full h-72 md:w-1/2 md:h-1/2 image-block'>
+      <div className='flex flex-col justify-start items-end w-full h-72 md:w-1/2 md:h-1/2 image-block'>
         <div className='flex relative rounded-3xl linear-bg-white shadow-white w-4/5 h-full max-w-sm image-block '>
           <Image src={Me} alt='profile-photo' className='flex rounded-3xl absolute bottom-0 left-0 w-full max-w-sm' />
         </div>
